97af5d199b61ca7585e43c039caaa94ca5d8267e
BasilAk-cyber <aubasil07@gmail.com>
2025-09-20 06:05:56 +0100

    First Commit

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+        PromptUserName();
+        string name = Console.ReadLine();
+        PromptUserNumber();
+        int number = int.Parse(Console.ReadLine());
+        int squarenum = SquareNumber(number);
+        DisplayResult(name, squarenum);
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program");
+        }
+
+        static void PromptUserName()
+        {
+            Console.Write("Please Enter your name: ");
+        }
+
+        static void PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+        }
+
+        static void DisplayResult(string name, int number)
+        {
+            Console.WriteLine($"Brother {name}, the square of your number is {number}");
+        }
+
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
     }
 }
